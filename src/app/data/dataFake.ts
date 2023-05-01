982b999a48c6b831086205cc74ae6d2b01184074
miguel nunes <miguel.npa@gmail.com>
2023-05-01 15:57:54 -0300

    content-created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"NOVO HOMEM DE FERRO EM 3D",
+    "description":"Marvel Anuncia novo homem de ferro",
+    "photoCover":'https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg'
+  },
+  {
+    "id":"2",
+    "title":"Novas Series da Marvel foram adiadas",
+    "description":"lorem ipsum lorem lorem lorem lorem lorem",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+]
